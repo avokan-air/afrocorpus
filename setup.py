2143537306457c9a8a0feb666a9bfd54cf0c46ac
Ola ABOUBAKAR <avokan.ola@gmail.com>
2026-01-22 18:31:18 +0100

    autocrlf

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="afrocorpus",
-    version="0.1.0",
+    version="0.1.1",
     author="Ola ABOUBAKAR",
     author_email="ola@avokan.com",
     description="A Python package for managing and querying African text corpora",
